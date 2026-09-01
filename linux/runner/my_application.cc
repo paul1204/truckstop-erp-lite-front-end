cb6a40b12b99f4bf5e18378c92c795bb8e21f676
Paramveer Singh <46756848+paul1204@users.noreply.github.com>
2026-09-01 16:00:06 -0400

    Named Stop #1

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "self_improvement_app");
+    gtk_header_bar_set_title(header_bar, "Truck Stop 1 LTE ERP");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "self_improvement_app");
+    gtk_window_set_title(window, "Truck Stop 1 LTE ERP");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
